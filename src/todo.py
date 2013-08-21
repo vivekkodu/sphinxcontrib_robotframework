188abafa64075fed6835e5050dbee661e36eab6a
vivekkodu <1990vivekkumarverma@gmail.com>
2013-08-21 12:10:54 +0530

    Update todo.py

--- src/todo.py
+++ src/todo.py
@@ -115,4 +115,39 @@ def process_todo_nodes(app, doctree, fromdocname):
 
 def run_robot(app,docname,source):
     from robot import run
-    run(docname)
+    from docutils.core import publish_cmdline
+    import docutils.core
+    import tempfile
+    import os
+    #run(os.path.join(app.srcdir, docname) + ".rst")
+    doctree = docutils.core.publish_doctree(open('C:/Users/viviverm/Downloads/sphinxcontrib_robotframework/docs/example.rst').read())
+    def collect_robot_nodes(node, collected = []):
+                robot_tagname = 'literal_block'
+                robot_classes = ['code', 'robotframework']
+                is_robot_node = (
+                    node.tagname == robot_tagname
+                    and node.attributes.get('classes') == robot_classes
+                )
+                if is_robot_node:
+                    collected.append(node)
+                else:
+                    for node in node.children:
+                        collected = collect_robot_nodes(node, collected)
+                return collected
+
+    robot_nodes = collect_robot_nodes(doctree)
+
+    if robot_nodes:
+        robot_data = "\n\n".join([node.rawsource for node in robot_nodes])
+        print(robot_data)
+        txtfile = open("C:/Users/viviverm/Downloads/sphinxcontrib_robotframework/docs/temp.robot","wb")
+        txtfile.write(robot_data)
+        txtfile.seek(0)
+        #run(txtfile)
+
+    if os.path.getsize('C:/Users/viviverm/Downloads/sphinxcontrib_robotframework/docs/temp.robot')>0:
+        run('C:/Users/viviverm/Downloads/sphinxcontrib_robotframework/docs/temp.robot')
+
+
+    #need to delete content of temp.robot
+    #don't know why extensions has to be changed in conf.py for each subsequent run
