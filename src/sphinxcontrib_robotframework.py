40c40d9dd8abfd927381d31272a6ebcd2b65e0d6
Asko Soukka <asko.soukka@iki.fi>
2013-09-02 19:23:19 +0300

    Rename option; Enable simplified way, since it seems to work

--- src/sphinxcontrib_robotframework.py
+++ src/sphinxcontrib_robotframework.py
@@ -1,54 +1,50 @@
+# -*- coding: utf-8 -*-
+
+import os
+import robot
+
+
 def setup(app):
-    app.add_config_value('sphinxcontrib_include',True,True)
-    app.connect('doctree-resolved',run_robot)
+    app.add_config_value('sphinxcontrib_robotframework_enabled', True, True)
+    app.connect('doctree-resolved', run_robot)
 
 
-from docutils import nodes
-
-def purge_todos(app, env, docname):
-    if not hasattr(env, 'todo_all_todos'):
+def run_robot(app, doctree, docname):
+    if not app.config.sphinxcontrib_robotframework_enabled:
         return
-    env.todo_all_todos = [todo for todo in env.todo_all_todos
-                          if todo['docname'] != docname]
 
+    robot.run(os.path.join(app.srcdir, docname) + ".rst")
 
+    return
 
+    # XXX: The approach after this is not required, if we can execute
+    # the above robot.run-call without error...
 
-def run_robot(app,doctree,docname):
-    if app.config.sphinxcontrib_include:
-        from robot import run
-        from docutils.core import publish_cmdline
-        import docutils.core
-        import tempfile
-        import os
-        #run(os.path.join(app.srcdir, docname) + ".rst")
-        #doctree = docutils.core.publish_doctree(open('C:/Users/viviverm/Downloads/sphinxcontrib_robotframework/docs/example.rst').read())
-        def collect_robot_nodes(node, collected = []):
-                    robot_tagname = 'literal_block'
-                    robot_classes = ['code', 'robotframework']
-                    is_robot_node = (
-                        node.tagname == robot_tagname
-                        and node.attributes.get('classes') == robot_classes
-                    )
-                    if is_robot_node:
-                        collected.append(node)
-                    else:
-                        for node in node.children:
-                            collected = collect_robot_nodes(node, collected)
-                    return collected
+    def collect_robot_nodes(node, collected=[]):
+        robot_tagname = 'literal_block'
+        robot_classes = ['code', 'robotframework']
+        is_robot_node = (
+            node.tagname == robot_tagname
+            and node.attributes.get('classes') == robot_classes
+        )
+        if is_robot_node:
+            collected.append(node)
+        else:
+            for node in node.children:
+                collected = collect_robot_nodes(node, collected)
+        return collected
 
-        robot_nodes = collect_robot_nodes(doctree)
+    robot_nodes = collect_robot_nodes(doctree)
 
-        if robot_nodes:
-            robot_data = "\n\n".join([node.rawsource for node in robot_nodes])
-            print(robot_data)
-            txtfile = open("C:/Users/viviverm/Downloads/sphinxcontrib_robotframework/docs/temp.robot","wb")
-            txtfile.write(robot_data)
-            txtfile.seek(0)
-            #run(txtfile)
+    if robot_nodes:
+        robot_data = "\n\n".join([node.rawsource for node in robot_nodes])
+        print(robot_data)
+        txtfile = open("C:/Users/viviverm/Downloads/sphinxcontrib_robotframework/docs/temp.robot","wb")
+        txtfile.write(robot_data)
+        txtfile.seek(0)
+        #run(txtfile)
 
-        if os.path.getsize('C:/Users/viviverm/Downloads/sphinxcontrib_robotframework/docs/temp.robot')>0:
-            run('C:/Users/viviverm/Downloads/sphinxcontrib_robotframework/docs/temp.robot')
+    if os.path.getsize('C:/Users/viviverm/Downloads/sphinxcontrib_robotframework/docs/temp.robot')>0:
+        run('C:/Users/viviverm/Downloads/sphinxcontrib_robotframework/docs/temp.robot')
 
-
-        #need to delete content of temp.robot or crete named temporary file
+    #need to delete content of temp.robot or crete named temporary file
