e6f3a5805e2a488fd5f3790ef3b2f48b5cec1efc
Asko Soukka <asko.soukka@iki.fi>
2013-09-02 20:01:18 +0300

    Refactor: Silence robot output; Change working directory during tests

--- src/sphinxcontrib_robotframework.py
+++ src/sphinxcontrib_robotframework.py
@@ -2,6 +2,8 @@
 
 import os
 import robot
+import tempfile
+import StringIO
 
 
 def setup(app):
@@ -13,19 +15,20 @@ def run_robot(app, doctree, docname):
     if not app.config.sphinxcontrib_robotframework_enabled:
         return
 
-    robot.run(os.path.join(app.srcdir, docname) + ".rst")
+    # XXX: This does not work, because it would run Docutils within Docutils
+    # with unexpected consequences like: AttributeError: Values instance has no
+    # attribute 'env'
 
-    return
-
-    # XXX: The approach after this is not required, if we can execute
-    # the above robot.run-call without error...
+    # import os
+    # robot.run(os.path.join(app.srcdir, docname) + ".rst")
 
     def collect_robot_nodes(node, collected=[]):
         robot_tagname = 'literal_block'
-        robot_classes = ['code', 'robotframework']
+        robot_classes = frozenset(['code', 'robotframework'])
         is_robot_node = (
             node.tagname == robot_tagname
-            and node.attributes.get('classes') == robot_classes
+            and robot_classes <= frozenset(
+                node.attributes.get('classes'))
         )
         if is_robot_node:
             collected.append(node)
@@ -33,18 +36,36 @@ def run_robot(app, doctree, docname):
             for node in node.children:
                 collected = collect_robot_nodes(node, collected)
         return collected
-
     robot_nodes = collect_robot_nodes(doctree)
 
-    if robot_nodes:
-        robot_data = "\n\n".join([node.rawsource for node in robot_nodes])
-        print(robot_data)
-        txtfile = open("C:/Users/viviverm/Downloads/sphinxcontrib_robotframework/docs/temp.robot","wb")
-        txtfile.write(robot_data)
-        txtfile.seek(0)
-        #run(txtfile)
+    if not robot_nodes:
+        return
 
-    if os.path.getsize('C:/Users/viviverm/Downloads/sphinxcontrib_robotframework/docs/temp.robot')>0:
-        run('C:/Users/viviverm/Downloads/sphinxcontrib_robotframework/docs/temp.robot')
+    robot_data = "\n\n".join([node.rawsource for node in robot_nodes])
 
-    #need to delete content of temp.robot or crete named temporary file
+    robot_file = tempfile.NamedTemporaryFile(suffix='.robot')
+    robot_file.write(robot_data.encode('utf-8'))
+    robot_file.flush()  # flush buffer into file
+
+    # Change the current working directory to app.srcdir:
+    cwd = os.getcwd()
+    os.chdir(app.srcdir)
+    stdout = StringIO.StringIO()
+
+    options = {
+        'outputdir': app.srcdir,
+        'stdout': stdout,
+        'output': 'NONE',
+        'log': 'NONE',
+        'report': 'NONE'
+    }
+    robot.run(robot_file.name, **options)
+
+    # Change the current working directory back:
+    os.chdir(cwd)
+
+    # TODO: Print the output only, if there were errors
+    stdout.seek(0)
+    print stdout.read()
+
+    robot_file.close()  # close file (and delete it, because it is a tempfile)
