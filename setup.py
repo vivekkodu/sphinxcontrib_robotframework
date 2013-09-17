f056733ac0b611526db02a02e4866ac29b7165ab
Asko Soukka <asko.soukka@iki.fi>
2013-09-17 07:20:35 +0300

    Preparing release 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sphinxcontrib-robotframework',
-    version='0.1.0',
+    version='0.2.0',
     description='Robot Framework plugin for Sphinx',
     url='http://github.com/vivekkodu/sphinxcontrib_robotframework',
     author='Vivek',
