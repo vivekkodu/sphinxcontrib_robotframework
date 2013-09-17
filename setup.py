ebb8a2fa7ecc65de22d2e8aa59ba16a4d4f1b8ae
Asko Soukka <asko.soukka@iki.fi>
2013-09-17 07:18:29 +0300

    Back to development: 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sphinxcontrib_robotframework',
-    version='0.1.0',
+    version='0.1.1.dev0',
     description='Robot Framework plugin for Sphinx',
     url='http://github.com/vivekkodu/sphinxcontrib_robotframework',
     author='Vivek',
