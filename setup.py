2dff0643454fb138987f5377be8c6875a585e10b
Asko Soukka <asko.soukka@iki.fi>
2013-09-21 14:51:15 +0300

    Preparing release 0.3.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sphinxcontrib-robotframework',
-    version='0.3.1.dev0',
+    version='0.3.1',
     description='Robot Framework plugin for Sphinx',
     long_description=(open("README.rst").read() + "\n" +
                       open("CHANGES.txt").read()),
